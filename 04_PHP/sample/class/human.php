16be97e2b2ec7f900cb50241d611424ce3cc5b04
Ryotaro Mastsuo <m.ryotaro0905@gmail.com>
2022-02-25 22:38:38 +0900

    Create human.php

---
+++ 04_PHP/sample/class/human.php
@@ -0,0 +1,36 @@
+<?php
+
+class BusinessMan extends Human {
+    private $work;
+
+    function __construct($lastName, $fistName, $age, $hobby, $work){
+        parent::__construct($lastName, $fistName, $age, $hobby);
+        $this->work =  $work;
+    }
+
+    function sayBusinessHello() {
+        echo "こんにちは私の名前は" . $this->lastName . " " . $this->fistName ."です" . $this->work."の仕事をしています".PHP_EOL;
+    }
+}
+
+class Human {
+    protected $lastName;
+    protected $fistName;
+    protected $age;
+    protected $hobby;
+
+    function __construct($lastName, $fistName, $age, $hobby){
+        $this->lastName = $lastName;
+        $this->fistName = $fistName;
+        $this->age = $age;
+        $this->hobby =  $hobby;
+    }
+
+    function sayHello() {
+        echo "こんにちは私の名前は" . $this->lastName . " " . $this->fistName ."です".PHP_EOL;
+    }
+}
+
+$matsuo2 = new BusinessMan("まつお", "りょうたろう", 34, "ばすけ", "エンジニア");
+$matsuo2->sayHello();
+$matsuo2->sayBusinessHello();
